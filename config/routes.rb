5249f0c25b1a58997889158613a5b81e204c4d25
Dipen Chauhan <get.dipen@gmail.com>
2026-08-12 17:27:43 -0500

    added devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations",
+    unlocks: "users/unlocks",
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
